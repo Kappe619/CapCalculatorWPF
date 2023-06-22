1eec80fffc866adbee94e2fdb7ffdd9199d8c7fd
Kappe619 <kappe619@gmail.com>
2023-06-22 20:58:48 +0200

    Basic App

---
+++ CapCalculator/CardSelectionControl.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace CapCalculator
+{
+    public partial class CardSelectionControlWithImage : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Level
+        {
+            get { return (int)GetValue(LevelProperty); }
+            set { SetValue(LevelProperty, value); }
+        }
+        public int SelectedIndex
+        {
+            get { return (int)GetValue(SelectedIndexProperty); }
+            set { SetValue(SelectedIndexProperty, value); }
+        }
+
+        public int Result
+        {
+            get { return (int)GetValue(ResultProperty); }
+            set { SetValue(ResultProperty, value); }
+        }
+
+        public static readonly DependencyProperty ResultProperty =
+            DependencyProperty.Register("Result", typeof(int), typeof(CardSelectionControlWithImage), new PropertyMetadata(0));
+
+        public static readonly DependencyProperty LevelProperty =
+            DependencyProperty.Register("Level", typeof(int), typeof(CardSelectionControlWithImage), new PropertyMetadata(4, new PropertyChangedCallback(OnChanged)));
+
+        public static readonly DependencyProperty SelectedIndexProperty =
+            DependencyProperty.Register("SelectedIndex", typeof(int), typeof(CardSelectionControlWithImage), new PropertyMetadata(0));
+
+        private static void OnChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            CardSelectionControlWithImage control = (CardSelectionControlWithImage)d;
+            control.Recalculate();
+        }
+
+        public void Recalculate()
+        {
+            if (upDown != null)
+            {
+                if (SelectedIndex == 0)
+                {
+                    if (Level < 4) { Level = 4; }
+                    upDown.Minimum = 4;
+                    Result = CalcCommon(Level);
+                }
+
+                else if (SelectedIndex == 1)
+                {
+                    if (Level < 3) { Level = 3; }
+                    upDown.Minimum = 3;
+                    Result = CalcRare(Level);
+                }
+
+                else if (SelectedIndex == 2)
+                {
+                    if (Level < 2) { Level = 2; }
+                    upDown.Minimum = 2;
+                    Result = CalcEpic(Level);
+                }
+
+                else if (SelectedIndex == 3)
+                {
+                    upDown.Minimum = 1;
+                    Result = CalcLegendary(Level);
+                }
+            }
+        }
+
+        public CardSelectionControlWithImage()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            Recalculate();
+        }
+
+        //Upgrade costs, array index = card level
+        private int[] commonCosts = { 0, 0, 0, 0, 0, 100, 275, 575 };
+        private int[] rareCosts = { 0, 0, 0, 0, 125, 325, 650, 1075 };
+        private int[] epicCosts = { 0, 0, 0, 175, 425, 800, 1275, 1825 };
+        private int[] legendaryCosts = { 0, 0, 200, 475, 875, 1375, 1950, 2575 };
+
+        int CalcCommon(int level)
+        {
+            return commonCosts[level];
+        }
+
+        int CalcRare(int level)
+        {
+            return rareCosts[level];
+        }
+
+        int CalcEpic(int level)
+        {
+            return epicCosts[level];
+        }
+
+        int CalcLegendary(int level)
+        {
+            return legendaryCosts[level];
+        }
+    }
+}
---
+++ CapCalculator/MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CapCalculator
+{
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int TotalCosts
+        {
+            get { return (int)GetValue(TotalCostsProperty); }
+            set { SetValue(TotalCostsProperty, value); }
+        }
+        public static readonly DependencyProperty TotalCostsProperty =
+            DependencyProperty.Register("TotalCosts", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+        public int CapsAvailable
+        {
+            get { return (int)GetValue(CapsAvailableProperty); }
+            set { SetValue(CapsAvailableProperty, value); }
+        }
+
+        public static readonly DependencyProperty CapsAvailableProperty =
+            DependencyProperty.Register("CapsAvailable", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+
+        public int Buffer
+        {
+            get { return (int)GetValue(BufferProperty); }
+            set { SetValue(BufferProperty, value); }
+        }
+
+        public static readonly DependencyProperty BufferProperty =
+            DependencyProperty.Register("Buffer", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+        public int Costs1
+        {
+            get { return (int)GetValue(Costs1Property); }
+            set { SetValue(Costs1Property, value); }
+        }
+
+        public static readonly DependencyProperty Costs1Property =
+            DependencyProperty.Register("Costs1", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+        public int Costs2
+        {
+            get { return (int)GetValue(Costs2Property); }
+            set { SetValue(Costs2Property, value); }
+        }
+
+        public static readonly DependencyProperty Costs2Property =
+            DependencyProperty.Register("Costs2", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+        public int Costs3
+        {
+            get { return (int)GetValue(Costs3Property); }
+            set { SetValue(Costs3Property, value); }
+        }
+
+        public static readonly DependencyProperty Costs3Property =
+            DependencyProperty.Register("Costs3", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+        public int Costs4
+        {
+            get { return (int)GetValue(Costs4Property); }
+            set { SetValue(Costs4Property, value); }
+        }
+
+        public static readonly DependencyProperty Costs4Property =
+            DependencyProperty.Register("Costs4", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+        public int Costs5
+        {
+            get { return (int)GetValue(Costs5Property); }
+            set { SetValue(Costs5Property, value); }
+        }
+
+        public static readonly DependencyProperty Costs5Property =
+            DependencyProperty.Register("Costs5", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+        public int Costs6
+        {
+            get { return (int)GetValue(Costs6Property); }
+            set { SetValue(Costs6Property, value); }
+        }
+
+        public static readonly DependencyProperty Costs6Property =
+            DependencyProperty.Register("Costs6", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+        public int Costs7
+        {
+            get { return (int)GetValue(Costs7Property); }
+            set { SetValue(Costs7Property, value); }
+        }
+
+        public static readonly DependencyProperty Costs7Property =
+            DependencyProperty.Register("Costs7", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+        public int Costs8
+        {
+            get { return (int)GetValue(Costs8Property); }
+            set { SetValue(Costs8Property, value); }
+        }
+
+        public static readonly DependencyProperty Costs8Property =
+            DependencyProperty.Register("Costs8", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+        public int Costs9
+        {
+            get { return (int)GetValue(Costs9Property); }
+            set { SetValue(Costs9Property, value); }
+        }
+
+        public static readonly DependencyProperty Costs9Property =
+            DependencyProperty.Register("Costs9", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+        public int Costs10
+        {
+            get { return (int)GetValue(Costs10Property); }
+            set { SetValue(Costs10Property, value); }
+        }
+
+        public static readonly DependencyProperty Costs10Property =
+            DependencyProperty.Register("Costs10", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+        public int Costs11
+        {
+            get { return (int)GetValue(Costs11Property); }
+            set { SetValue(Costs11Property, value); }
+        }
+
+        public static readonly DependencyProperty Costs11Property =
+            DependencyProperty.Register("Costs11", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+        public int Costs12
+        {
+            get { return (int)GetValue(Costs12Property); }
+            set { SetValue(Costs12Property, value); }
+        }
+
+        public static readonly DependencyProperty Costs12Property =
+            DependencyProperty.Register("Costs12", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(CostsChanged)));
+
+        public static void CostsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            MainWindow mainWindow = (MainWindow)d;
+
+            if (mainWindow != null)
+            {
+                mainWindow.TotalCosts =
+                    mainWindow.Costs1 +
+                    mainWindow.Costs2 +
+                    mainWindow.Costs3 +
+                    mainWindow.Costs4 +
+                    mainWindow.Costs5 +
+                    mainWindow.Costs6 +
+                    mainWindow.Costs7 +
+                    mainWindow.Costs8 +
+                    mainWindow.Costs9 +
+                    mainWindow.Costs10 +
+                    mainWindow.Costs11 +
+                    mainWindow.Costs12;
+
+                mainWindow.Buffer = mainWindow.CapsAvailable - mainWindow.TotalCosts;
+                if (mainWindow.Buffer < 0)
+                {
+                    mainWindow.BufferLbl.Background = Brushes.Red;
+                    return;
+                }
+                else
+                {
+                    mainWindow.BufferLbl.Background = Brushes.LightGreen;
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void AvailableCapsTb_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            //TODO: actuallisize buffer    
+            control1.Recalculate();
+        }
+
+        private void AvailableCapsTb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            //AvailableCapsTb.SelectAll();
+        }
+    }
+}
